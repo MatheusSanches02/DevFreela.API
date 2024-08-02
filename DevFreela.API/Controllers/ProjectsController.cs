6d758bd28d3eee58e19f85bd2bf632978b5afeae
MatheusSanches02 <ma12sanches@gmail.com>
2024-08-01 22:18:58 -0300

    Estrutura inicial e Padrao IOptions<>

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,70 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly FreelancerTotalCostingConfig _config;
+        public ProjectsController(IOptions<FreelancerTotalCostingConfig> options) 
+        {
+            _config = options.Value;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string search)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id) 
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post(CreateProjectInputModel model)
+        {
+            if(model.TotalCost < _config.Minimum ||  model.TotalCost > _config.Maximum)
+            {
+                return BadRequest("Custo fora dos limites");
+            }
+            return CreatedAtAction(nameof(GetById), new { id = 1}, model);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UpdateProjectInputModel model)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/complete")]
+        public IActionResult Complete(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, CreateProjectCommentInputModel model)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ DevFreela.API/Models/FreelancerTotalCostingConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace DevFreela.API.Models
+{
+    public class FreelancerTotalCostingConfig
+    {
+        public decimal Minimum {  get; set; }
+        public decimal Maximum { get; set; }
+    }
+}
